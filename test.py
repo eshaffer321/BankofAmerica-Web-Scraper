c8225f0a70bf3983b39b79c136bc4cc8eb1746a7
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2019-07-06 19:19:01 -0700

    removed test file

--- test.py
+++
@@ -1,7 +0,0 @@
-
-import requests
-
-
-data = [[], [], [{'merchant_name': 'SERAO ACADEMY', 'amount': '$99.00', 'category': 'Education: Education', 'date': '07/03/2019', 'description': 'SERAO ACADEMY 408-628-3662 CA', 'transaction_type': 'credit'}]]
-
-r = requests.post('http://localhost:3000/transactions', json=data)
